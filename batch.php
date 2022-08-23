07fa9e4881a1e79cf3d37c17a3ba03a1f777e25b
Jake Worrell <jake.worrell@chalkstring.com>
2022-08-24 00:36:24 +0100

    php script to generate a bunch of stls

---
+++ batch.php
@@ -0,0 +1,14 @@
+<?php
+
+for ($h=3;$h<=12;$h+=3) {
+    for ($w=1;$w<=4;$w++){
+        for ($d=$w;$d<=4;$d++){
+            $outfile = "./batchout/gridfinity-lite-{$w}x{$d}x{$h}.stl";
+            if (!file_exists($outfile)) {
+                echo $outfile . "\n";
+                exec("openscad gridfinity-rebuilt.scad -o {$outfile} -Dgridz={$w} -Dgridy={$d} -Dgridz={$h}");
+            }
+        }
+    }
+}
+
