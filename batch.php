9669841de5ff315ad7b745ae8e474a202f597623
Jake Worrell <jake.worrell@chalkstring.com>
2022-08-30 00:04:32 +0100

    generate more sizes

--- batch.php
+++ batch.php
@@ -1,11 +1,16 @@
 <?php
-
+$maxhorizontal = 6;
 for ($h=3;$h<=12;$h+=3) {
-    for ($w=1;$w<=4;$w++){
-        for ($d=$w;$d<=4;$d++){
-            $outfile = "./batchout/gridfinity-lite-{$w}x{$d}x{$h}.stl";
+    for ($w=1;$w<=$maxhorizontal;$w++){
+        for ($d=$w;$d<=$maxhorizontal;$d++){
+            $outdir = "./batchout/{$h}h/";
+            $outfile = $outdir . "gridfinity-lite-{$w}x{$d}x{$h}.stl";
+            if (!file_exists($outdir)){
+                mkdir($outdir);
+            }
             if (!file_exists($outfile)) {
                 echo $outfile . "\n";
+                $outfile = addslashes($outfile);
                 exec("openscad gridfinity-rebuilt.scad -o {$outfile} -Dgridx={$w} -Dgridy={$d} -Dgridz={$h}");
             }
         }
