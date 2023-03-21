0bad4be8d969be1a1a850d3349dc84df3223a198
Ruud Swinkels <ruudswinkels@hotmail.nl>
2023-03-21 22:59:44 +0100

    Isolate tested modules and add posibility to add dependency modules

--- tests/utility_test.py
+++ tests/utility_test.py
@@ -1,47 +1,59 @@
-from pathlib import Path
-from openscadtestrunner import ScadModule, OpenscadTestCase
+from openscadtestframework import ScadTestCase, Module, ModuleTest
 
 
-class UtilityTesttest(OpenscadTestCase):
+class UtilityTesttest(ScadTestCase):
 
     def test_block_base_hole_style_0(self) -> None:
+        tested_module = Module.from_file(
+            "block_base_hole", "gridfinity-rebuilt-utility.scad")
+        module_test = ModuleTest(tested_module, style_hole=1)
+        module_test.add_constants_file("gridfinity-constants.scad")
+        module_test.add_dependency(
+            Module("Test_mod", ['echo("something");'], []))
+
         self.scad_module_test(
-            ScadModule(Path("gridfinity-rebuilt-utility.scad"),
-                       "block_base_hole",
-                       style_hole=1), Path("tests/expected/test_block_base_hole_style_1.stl"))
+            module_test, "test_block_base_hole_style_1.stl")
 
 
-class UtilityTest(OpenscadTestCase):
-    def test_block_base_hole_style_0(self) -> None:
-        module = ScadModule(Path("gridfinity-rebuilt-utility.scad"),
-                            "block_base_hole",
-                            style_hole=1)
+class block_base(ScadTestCase):
+    def setUp(self) -> None:
+        self.module = Module.from_file(
+            "block_base_hole", "gridfinity-rebuilt-utility.scad")
+        self.module_test = ModuleTest(self.module)
+        self.module_test.add_constants_file("gridfinity-constants.scad")
+
+    def test_hole_style_0(self) -> None:
+        self.module_test.add_arguments(style_hole=0)
         self.scad_module_test(
-            module, Path("tests/expected/test_block_base_hole_style_1.stl"))
+            self.module_test, "test_block_base_hole_style_1.stl", keep_files=True)
 
-    def test_block_base_hole_style_1(self) -> None:
-        module = ScadModule(Path("gridfinity-rebuilt-utility.scad"),
-                            "block_base_hole", style_hole=1)
+    def test_hole_style_1(self) -> None:
+        self.module_test.add_arguments(style_hole=1)
         self.scad_module_test(
-            module, Path("tests/expected/test_block_base_hole_style_1.stl"))
+            self.module_test, "test_block_base_hole_style_1.stl")
 
-    def test_block_base_hole_style_2(self) -> None:
-        module = ScadModule(Path("gridfinity-rebuilt-utility.scad"),
-                            "block_base_hole", style_hole=2)
+    def test_hole_style_2(self) -> None:
+        self.module_test.add_arguments(style_hole=2)
         self.scad_module_test(
-            module, Path("tests/expected/test_block_base_hole_style_2.stl"))
+            self.module_test, "test_block_base_hole_style_2.stl")
 
-    def test_block_base_hole_style_3(self) -> None:
-        module = ScadModule(Path("gridfinity-rebuilt-utility.scad"),
-                            "block_base_hole", style_hole=3)
+    def test_hole_style_3(self) -> None:
+        self.module_test.add_arguments(style_hole=3)
+        self.module_test.add_dependency(Module.from_file(
+            "copy_mirror", "gridfinity-rebuilt-utility.scad"))
         self.scad_module_test(
-            module, Path("tests/expected/test_block_base_hole_style_3.stl"))
+            self.module_test, "test_block_base_hole_style_3.stl", keep_files=True)
 
 
-class BaseplateTest(OpenscadTestCase):
+class BaseplateTest(ScadTestCase):
 
     def test_cutter_weight(self) -> None:
-        module = ScadModule(Path("gridfinity-rebuilt-baseplate.scad"),
-                            "cutter_weight")
+        module = Module.from_file(
+            "cutter_weight", "gridfinity-rebuilt-baseplate.scad")
+
+        module_test = ModuleTest(module)
+        module_test.add_constants_file("gridfinity-constants.scad")
+        module_test.add_dependency(Module.from_file(
+            "pattern_circular", "gridfinity-rebuilt-utility.scad"))
         self.scad_module_test(
-            module, Path("tests/expected/test_cutter_weight.stl"))
+            module_test, "test_cutter_weight.stl")
