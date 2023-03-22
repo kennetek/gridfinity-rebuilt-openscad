f4a6fe87e74c5ca06a394f9883f5aa50e30d8d5f
Ruud Swinkels <ruudswinkels@hotmail.nl>
2023-03-22 22:25:56 +0100

    add rounded_rectangle_tets

--- tests/test_unit_utility.py
+++ tests/test_unit_utility.py
@@ -29,3 +29,20 @@ class block_base(ScadTestCase):
             "copy_mirror", "gridfinity-rebuilt-utility.scad"))
         self.scad_module_test(
             self.module_test, "test_block_base_hole_style_3.stl")
+
+
+class rounded_rectangle(ScadTestCase):
+    def setUp(self) -> None:
+        self.module = Module.from_file(
+            "rounded_rectangle", "gridfinity-rebuilt-utility.scad")
+        self.module_test = ModuleTest(self.module)
+
+    def test_basic(self) -> None:
+        self.module_test.add_arguments(50, 35, 20, 5)
+        self.scad_module_test(
+            self.module_test, "test_rounded_rectangle_basic.stl")
+
+    def test_basic_different(self) -> None:
+        self.module_test.add_arguments(30, 60, 50, 2)
+        self.scad_module_test(
+            self.module_test, "test_rounded_rectangle_basic_different.stl", keep_files=True)
