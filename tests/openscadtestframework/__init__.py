5ec974812c2b17ed3d2e44460d6b0009fae1cfd4
Ruud Swinkels <ruudswinkels@hotmail.nl>
2023-03-31 13:37:38 +0200

    fix linting errors

--- tests/openscadtestframework/__init__.py
+++ tests/openscadtestframework/__init__.py
@@ -1,3 +1,6 @@
 # __init__.py
+
+__all__ = ["ScadIntegrationTestCase", "IntegrationTest",
+           "ScadModuleTestCase", "Module", "ModuleTest", "Cube", "Square"]
 from .openscadtestframework import ScadIntegrationTestCase, IntegrationTest, ScadModuleTestCase, Module, ModuleTest
 from .basic_modules import Cube, Square
